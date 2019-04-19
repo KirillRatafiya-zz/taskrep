f4d7920bab4574ff31d95e0980e29004ec29b39a
KirillRatafiya <47858801+KirillRatafiya@users.noreply.github.com>
2019-04-19 18:49:17 +0300

    Add files via upload

---
+++ ConsoleAppfForGit/ConsoleAppfForGit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppfForGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****Hello World****");
+            Console.WriteLine("***Add some changes***");
+            Console.ReadLine();
+        }
+    }
+}
