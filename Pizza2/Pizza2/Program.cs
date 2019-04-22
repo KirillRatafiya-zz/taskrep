94e2223a2423d87a6bf5e6209433c1479d4e4497
KirillRatafiya <KirillRatafiya@coherentsolutions.com>
2019-04-22 11:28:48 +0300

    Pizza Commit

---
+++ Pizza2/Pizza2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Cоздать класс Пицца, которая умела бы печатать свои ингридиенты, 
+//и в которую эти сами ингридиенты можно было бы добавлять по ходу выполнения программы.
+
+
+namespace Pizza2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ketchup
+            // sausage
+
+            var pizza = new Pizza();
+            pizza.Ingredients.Add("ketchup");
+            pizza.Ingredients.Add("sausage");
+
+
+            //foreach(string i in pizza.Ingredients)
+            //Console.WriteLine(i);
+
+            for(int i = 0; i < pizza.Ingredients.Count; i++)
+            {
+                Console.WriteLine("Added ingredients: {0}", pizza.Ingredients[i]);
+            }
+            
+
+            Console.ReadLine();
+        }
+
+        class Pizza
+        {
+            public List<string> Ingredients { get; set; } = new List<string>();
+        }
+        
+    }
+}
